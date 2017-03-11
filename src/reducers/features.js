9eeb0d5946fc8ad2217bc42209d02b3abd518fc5
pgaret <peregringaret@gmail.com>
2017-03-11 18:12:37 -0500

    Can now add features again

--- src/reducers/features.js
+++ src/reducers/features.js
@@ -1,8 +1,9 @@
 export default function features(state = [{setting: false}], action){
-  let state_features = state[0]
+  let state_features = state.features
   let new_state = state
   switch(action.type){
     case 'GET_FEATURES':
+      state_features = state[0]
       state_features.features = action.payload
       return Object.assign({}, state[0], state_features)
     case 'UPDATE_FEATURES':
@@ -24,10 +25,12 @@ export default function features(state = [{setting: false}], action){
       console.log(state_features)
       return {features: state_features}
     case 'CREATE_FEATURE':
+      state_features = state.features
       state_features.push(action.payload)
       state_features.setting = false
       return {features: state_features}
     case 'SETTING_FEATURE_NAME':
+      state_features = state.features
       state_features.setting = !state.features.setting
       return {features: state_features}
     default:
